2b74884941ebe18b7e30d1af11a27062c04e79c2
Robin Peter <robin.peter.vkm@gmail.com>
2021-02-11 22:04:04 -0400

    NOtes

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 const myName: string = 'Robin';
 /* string --> type annotation */
+/* 
+string      eg- 'apple', 'banana' , 'templte string'
+number      eg- 3 , 0 ,  10.00001 , -9 
+boolean     eg- true , false 
+undefined   eg- undefined
+null        eg- null
+any         eg- 10 , false , 'hi there!' ,  {color:'red'}
+ */
